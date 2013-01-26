09b10ca2521198e6cad05bb4f84aa30e2d25e14e
Isis Bulte <isis.bulte@ppw.kuleuven.be>
2013-01-26 00:00:00 +0000

    version 0.1

--- R/graph.extract.R
+++ R/graph.extract.R
@@ -1,6 +1,56 @@
+## The functions imagematrix', 'rgb2grey', 'clipping' and 'read.jpeg' are adopted from the ReadImages package, which has been orphaned and archived by CRAN.
+
 graph.extract<-function(MT, refX, refY, save="no", image=read.jpeg(file.choose())){
+
+imagematrix <- function(mat, type=NULL, ncol=dim(mat)[1], nrow=dim(mat)[2],
+                        noclipping=FALSE) {
+  if (is.null(dim(mat)) && is.null(type)) stop("Type should be specified.")
+  if (length(dim(mat)) == 2 && is.null(type)) type <- "grey"
+  if (length(dim(mat)) == 3 && is.null(type)) type <- "rgb"
+  if (type != "rgb" && type != "grey") stop("Type is incorrect.")
+  if (is.null(ncol) || is.null(nrow)) stop("Dimension is uncertain.")
+  imgdim <- c(ncol, nrow, if (type == "rgb") 3 else NULL)
+  if (length(imgdim) == 3 && type == "grey") {
+    # force to convert grey image
+    mat <- rgb2grey(mat)
+  }
+  if (noclipping == FALSE && ((min(mat) < 0) || (1 < max(mat)))) {
+    warning("Pixel values were automatically clipped because of range over.") 
+    mat <- clipping(mat)
+  }
+  mat <- array(mat, dim=imgdim)
+  attr(mat, "type") <- type
+  class(mat) <- c("imagematrix", class(mat))
+  mat
+}
+
+rgb2grey <- function(img, coefs=c(0.30, 0.59, 0.11)) {
+  if (is.null(dim(img))) stop("image matrix isn't correct.")
+  if (length(dim(img))<3) stop("image matrix isn't rgb image.")
+  imagematrix(coefs[1] * img[,,1] + coefs[2] * img[,,2] + coefs[3] * img[,,3],
+              type="grey")
+}
+clipping <- function(img, low=0, high=1) {
+  img[img < low] <- low
+  img[img > high] <- high
+  img
+}
   
-  library(ReadImages)
+read.jpeg <- function(filename) {
+  res <- .C("get_imagesize_of_JPEG_file", as.character(filename),
+            width=integer(1), height=integer(1), depth=integer(1),
+            ret=integer(1), PACKAGE="SCVA")
+  if (res$ret < 0)
+    stop(if (res$ret==-1) "Can't open file." else "Internal error")
+  imgtype <- if (res$depth == 1) "grey" else "rgb"
+  imgdim <- c(res$height, res$width, if (res$depth == 3) res$depth else NULL)
+  res <- .C("read_JPEG_file", as.character(filename),
+            image=double(res$width * res$height * res$depth),
+            ret=integer(1), PACKAGE="SCVA")
+  img <- array(res$image, dim=imgdim)
+  imagematrix(img/255, type=imgtype)
+}
+ 
   plot(image)
   refpoints <- locator(n = 4, type = 'p', pch = 4, col = 'blue', lwd = 2)
   refpoints <- as.data.frame(refpoints)	
@@ -21,3 +71,4 @@ graph.extract<-function(MT, refX, refY, save="no", image=read.jpeg(file.choose()
   return(rounded)
 
 }
+
